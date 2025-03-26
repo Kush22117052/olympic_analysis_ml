3518cd6c68645d71812a827638d2ecd5a88e052e
Kushagra Kumar Sinha <sinhakushagra04@gmail.com>
2025-03-26 21:41:12 +0530

    new change

--- setup.sh
+++ setup.sh
@@ -11,5 +11,10 @@
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
+if [ -z "$PORT" ]; then
+  PORT=8501
+fi
+
+# Run the Streamlit app with the correct server port and address
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
 
