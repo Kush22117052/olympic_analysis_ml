71c6856961bc7b8a2856eee3141f7c0bfacc87b5
Kushagra Kumar Sinha <sinhakushagra04@gmail.com>
2025-03-26 18:12:12 +0530

    Update setup.sh to use Render's PORT environment variable

--- setup.sh
+++ setup.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 # Ensure ~/.streamlit directory exists
-mkdir -p ~/.streamlit/
+# mkdir -p ~/.streamlit/
 
-# Create the Streamlit config file with proper environment variable for $PORT
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-" > ~/.streamlit/config.toml
+# Create the Streamlit config file
+# echo "[server]
+# headless = true
+# enableCORS = false
+# " > ~/.streamlit/config.toml
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
-
-# Make sure setup.sh is executable
-chmod +x setup.sh
+streamlit run app.py --server.port $PORT
